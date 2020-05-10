d0a88c371dc5ec40be2025dc9f1d9dd960c9e31c
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-05-10 23:55:32 +0700

    Add: pagination book page

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -3,8 +3,22 @@ const Book = require('../model/book.model');
 
 //book index
 module.exports.index = async (req, res) => {
+    let page = req.query.page || 1;
     let books = await Book.find();
-    res.render('./book/index', {books});
+    //Number of items on the page
+    let perPage = 6;
+    // total page
+    let numPage = Math.ceil(books.length / 6);
+    //Starting at item
+    let n = (page - 1) * perPage;
+    //Ending at item -1
+    let m = page * perPage;
+
+    res.render('./book/index', {
+        books: books.slice(n, m),
+        numPage: numPage,
+        page: parseInt(page)
+    });
 }
 //create book
 module.exports.create = (req, res) => {
@@ -14,7 +28,7 @@ module.exports.create = (req, res) => {
 module.exports.postCreate = (req, res) => {
     let book = new Book(req.body);
     book.save()
-     .then( _ => console.log('Book created'))
-     .catch(err => console.log(err));
+        .then(_ => console.log('Book created'))
+        .catch(err => console.log(err));
     res.redirect('/book');
 }
--- model/book.model.js
+++ model/book.model.js
@@ -1,11 +1,23 @@
 const {Schema, model} = require('mongoose');
 const bookSchema = new Schema({
+    image: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
     },
     description: {
         type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
         required: true
     }
 });
