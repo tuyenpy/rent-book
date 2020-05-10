7f8868b1848fe9af0edf3ec74043e91b308dcebf
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-05-10 09:14:15 +0700

    Refactor code

--- route/user.route.js
+++ route/user.route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer({dest: 'publics/uploads/users'});
 const controller = require('../controller/user.controller');
+const validate = require('../validate/user.validate');
 
 //create user
 router.get('/create', controller.create);
-router.post('/create', upload.single('avatar'), controller.postCreate);
+router.post('/create', validate.create, upload.single('avatar'), controller.postCreate);
 
 module.exports = router;
--- validate/user.validate.js
+++ validate/user.validate.js
@@ -1,7 +1,7 @@
 module.exports.create = (req, res, next) => {
     let errors = [];
     // name contains only characters a-zA-Z and max 5 char
-    let nameValidate = /^([a-zA-Z]){3,5}$/ig;
+    let nameValidate = /^([a-zA-Z]){3,10}$/ig;
     // phone contains onpy digit and max 10 char
     let phoneValidate = /^(\d){9,10}$/ig
 
@@ -9,16 +9,15 @@ module.exports.create = (req, res, next) => {
 
     // validate data in form
     if (!name || !phone || !email || !password) {
-        error = "Please fill out all fields";
-        res.render('./user/create', { error });
+        errors.push("Please fill out all fields");
     }
     //validate name, phone, password
     if (nameValidate.test(name) === false) {
-        errors.push("name contains only characters a-zA-Z and min 3 char, max 5 char");
+        errors.push("Name contains only characters a-zA-Z and min 3 char, max 5 char");
     }
 
     if (phoneValidate.test(phone) === false) {
-        errors.push("phone contains onpy digit and min 9 and max 10 digit");  
+        errors.push("Phone contains onpy digit and min 9 and max 10 digit");  
     }
 
     //display errors for user
