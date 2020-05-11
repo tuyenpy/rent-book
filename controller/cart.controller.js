1869de38805f7fc0c4906666330ee06e0decfe3a
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-05-11 16:04:46 +0700

    Fix: delete all book in cart

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -2,27 +2,27 @@ const User = require('../model/user.model');
 const Book = require('../model/book.model');
 
 module.exports.index = async (req, res) => {
+    //Check query
+    let q = req.query.q;
+    //Retrieve user information
     let user = res.locals.user;
+    //Retrieve book information
     let books = await Promise.all(user.cart.map(async id => {
         return await Book.findById(id)
     }));
+    //If the query equals reset, all books in the basket will be deleted
+    if (q === 'reset') {
+        console.log(q);
+        await User.findOneAndUpdate({
+            _id: user._id
+        }, {
+            cart: []
+        }, {
+            new: true
+        })
+    }
+
     res.render('./cart/index', {
         books
     });
-}
-
-module.exports.reset = (req, res) => {
-    let user = res.locals.user;
-
-    User.findOneAndUpdate({
-        _id: user._id
-    }, {
-        cart: []
-    }, {
-        new: true
-    })
-        .then()
-        .catch(err => console.log(err));
-
-    res.redirect('/');
 }
--- route/cart.route.js
+++ route/cart.route.js
@@ -5,7 +5,5 @@ const controller = require('../controller/cart.controller');
 //View all books in cart
 router.get('/', controller.index);
 
-//Delete all books in the cart
-router.get('/reset', controller.reset);
 
 module.exports = router;
